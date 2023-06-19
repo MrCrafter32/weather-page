1cdd61e4337355de30146f5c4c9602e7614893aa
MrCrafter32 <jagadeesh.d125@gmail.com>
2023-06-19 21:06:57 +0530

    v1.0

---
+++ action.php
@@ -0,0 +1,63 @@
+<?php
+$name=$_POST['name'];
+$key='a359a5eaaced4d60a02140512231906';
+$url='http://api.weatherapi.com/v1/current.json?key='.$key.'&q='.$name.'&aqi=no';
+$response=file_get_contents($url);
+$data=json_decode($response,true);
+?>
+
+<html>
+<head>
+<title>Weather</title>
+</head>
+<body>
+<h1>Weather</h1>
+<form action="action.php" method="post">
+<input type="text" name="name" placeholder="Enter City Name">
+<input type="submit" name="submit" value="submit">
+</form>
+<?php
+if(isset($_POST['submit']))
+{
+?>
+<table border="1">
+<tr>
+<th>City</th>
+<th>Region</th>
+<th>Country</th>
+<th>Local Time</th>
+<th>Temperature</th>
+<th>Condition</th>
+<th>Wind Speed</th>
+<th>Wind Direction</th>
+<th>Humidity</th>
+<th>Pressure</th>
+<th>Cloud</th>
+<th>UV</th>
+<th>Visibility</th>
+<th>Precipitation</th>
+</tr>
+<tr>
+
+<td><?php echo $data['location']['name'];?></td>
+<td><?php echo $data['location']['region'];?></td>
+<td><?php echo $data['location']['country'];?></td>
+<td><?php echo $data['location']['localtime'];?></td>
+<td><?php echo $data['current']['temp_c'];?>&deg;C</td>
+<td><?php echo $data['current']['condition']['text'];?></td>
+<td><?php echo $data['current']['wind_kph'];?>kph</td>
+<td><?php echo $data['current']['wind_dir'];?></td>
+<td><?php echo $data['current']['humidity'];?>%</td>
+<td><?php echo $data['current']['pressure_mb'];?>mb</td>
+<td><?php echo $data['current']['cloud'];?>%</td>
+<td><?php echo $data['current']['uv'];?></td>
+<td><?php echo $data['current']['vis_km'];?>km</td>
+<td><?php echo $data['current']['precip_mm'];?>mm</td>
+</tr>
+</table>
+<?php
+}
+?>
+</body>
+</html>
+
