7753c8e617f2f165ca28bd2eaab1ffe63d10b7a1
MrCrafter32 <jagadeesh.d125@gmail.com>
2023-06-19 22:36:05 +0530

    updated homepage with styles

--- index.php
+++ index.php
@@ -1,12 +1,43 @@
 <html>
-    <head>
 
-    </head>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
 
-    <body>
+<body>
+    <div class='d-flex justify-content-end pg-navbar'>
+        <nav class='navbar'>
+            <ul class='nav justify-content-end'>
+                <li class='nav-item'>
+                    <a class='nav-link' aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class='nav-item'>
+                    <a class='nav-link' aria-current="page" href="contact.php">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="header">
+        <h1>Enter your City Name</h1>
+
+
+    <div>
         <form action="action.php" method="get">
-            <input type="text" name="name" placeholder="Name">
+            <input  type="text" name="name" placeholder="Name">
             <button type="submit" name="submit">Submit</button>
         </form>
-    </body>
+         </div>
+    </div>
+
+
+
+
+    <div class="footer">
+        Powered by <a href="https://www.weatherapi.com">WeatherAPI</a>
+    </div>
+</body>
+
 </html>
